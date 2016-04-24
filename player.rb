0191001a5bed820e9c1d6914b30ab7b14835cfba
Dmitry Davydov <haudvd@gmail.com>
2016-04-24 11:18:39 +0400

    remove self

--- player.rb
+++ player.rb
@@ -17,16 +17,14 @@ class Player
 
   end
 
-  class << self
-    def our_hand(game_state)
-      our_team = game_state["players"].select { |team|
-        team["name"] == "RubyTeam"
-      }.first
-      our_team["hole_cards"]
-    end
+  def our_hand(game_state)
+    our_team = game_state["players"].select { |team|
+      team["name"] == "RubyTeam"
+    }.first
+    our_team["hole_cards"]
+  end
 
     # def rank_hand(our_hand(game_state))
 
     # end
-  end
 end
