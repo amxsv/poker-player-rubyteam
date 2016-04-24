8b93c1cd878fa624eab49bdfe705d9bcb0bd1ad9
Dmitry Davydov <haudvd@gmail.com>
2016-04-24 12:00:31 +0400

    call

--- player.rb
+++ player.rb
@@ -7,10 +7,23 @@ class Player
     "huiui"
   end
 
+  def call_bet(game_state)
+    in_action = game_state["in_action"]
+    players = game_state["players"]
+    bet = (game_state["current_buy_in"] - players[in_action]["bet"]) + 1
+    bet = bet > 101 ? 0 : bet
+
+    bet
+  end
+
   def bet_request(game_state)
     p game_state
     # get_coefficient(game_state) * 1000
-    300
+    call_bet(game_state)
+  rescue Exception => e
+    p "ERRORERROR, but we alive"
+    p e.inspect
+    0
   end
 
   def showdown(game_state)
