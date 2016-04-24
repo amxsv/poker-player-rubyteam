236c91462498b6e830311b608639c01881657c39
Maksim Slepukhov <maksim.slepukhov@gmail.com>
2016-04-24 14:35:59 +0400

    Add player test

--- player.rb
+++ player.rb
@@ -13,7 +13,14 @@ class Player
     in_action = game_state["in_action"]
     players = game_state["players"]
     bet = (game_state["current_buy_in"] - players[in_action]["bet"]) + 1
-    bet = bet > 51 ? 0 : bet
+    rank = rank_hand(game_state)
+    if (rank > 6)
+      bet = rank * Random.new.rand(69..72)
+    elsif (rank > 0)
+      bet = rank * Random.new.rand(18..23)
+    else
+      bet = bet > 51 ? 0 : bet
+    end
 
     bet
   end
@@ -44,10 +51,17 @@ class Player
   end
 
   def rank_hand(game_state)
-    HTTParty.get('http://rainman.leanpoker.org/rank',
+    full_cards = community_cards(game_state) + our_hand(game_state)
+    if full_cards.size < 5
+      return 0
+    end
+    puts full_cards.inspect
+
+    respond = HTTParty.get('http://rainman.leanpoker.org/rank',
                  headers: {'Content-Type' => 'application/x-www-form-urlencoded'},
-                 body: "cards=#{CGI.escape(our_hand(game_state).to_json)}"
+                 body: "cards=#{CGI.escape(full_cards.to_json)}"
                 )
+    JSON.parse(respond)['rank']
   end
 
   def pair?(cards)
